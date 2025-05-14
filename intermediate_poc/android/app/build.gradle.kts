b71d08792bcadadf9209a46236db50ca62e8f19f
SagarThoutu <sagarreddy1851998@gmail.com>
2025-05-14 15:07:04 +0530

    Update build.gradle.kts

--- intermediate_poc/android/app/build.gradle.kts
+++ intermediate_poc/android/app/build.gradle.kts
@@ -7,8 +7,8 @@ plugins {
 
 android {
     namespace = "com.example.intermediate_poc"
-    compileSdk = 33
-    ndkVersion = flutter.ndkVersion
+    compileSdk = 35
+    ndkVersion = "27.0.12077973"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
